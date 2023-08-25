98e97d17f4d621938cb3e6e3888cd7eab79986a9
Mizuinu30 <juansilva.dvm@gmail.com>
2023-08-25 14:38:39 +0000

    removed blank spaces

--- _print_env.c
+++ _print_env.c
@@ -1,24 +1,24 @@
 #include "shell.h"
 
 /**
-  * _print_env - Prints the environment built-in.
-  *
-  * Return: Nothing.
-  */
+ * _print_env - Prints the environment built-in.
+ *
+ * Return: Nothing.
+ */
 void _print_env(void)
 {
 	int i;
 	int j;
 
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        for (j = 0; environ[i][j] != '\0'; j++)
-        {
-            _putchar(environ[i][j]);
-        }
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		for (j = 0; environ[i][j] != '\0'; j++)
+		{
+			_putchar(environ[i][j]);
+		}
 
-        if (environ[i][0] != '\0')
-            _putchar('\n');
-    }
+		if (environ[i][0] != '\0')
+			_putchar('\n');
+	}
 }
 
